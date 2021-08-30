939fe04283bec38f9239cdebb5602243ea12b1cd
Mammuasyir <mammuasyir0101@gmail.com>
2021-08-30 09:07:19 +0700

    latihan pemantapan materi ke tujuh

---
+++ src/main/kotlin/Tugas7.kt
@@ -0,0 +1,22 @@
+fun jay(b: Int) :Int {
+    if (b % 2 == 0) {
+        println("$b adalah bilangan genap")
+    } else {
+        println("$b adalah bilangan ganjil")
+    }
+    return b
+}
+
+fun main() {
+  //  println(check( 3))
+    jay (2)
+}
+//fun check(a: Int) :String {
+  //  if (a % 2 == 0) {
+    //    return "$a adalah bilangan genap"
+   // } else {
+     //   return "$a adalah bilangan ganjil"
+
+    //}
+//}
+
